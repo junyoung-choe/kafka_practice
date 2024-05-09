2fd17484ab9499c59e30e5fda480b13a696d54e9
junyoung-choe <wjdghkswnsdu@naver.com>
2024-05-10 01:11:56 +0900

    feat: trigger api 추가

---
+++ server1/src/main/java/org/example/server1/Controller.java
@@ -0,0 +1,20 @@
+package org.example.server1;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class Controller {
+    private final Producer producer;
+    @PostMapping("kafka")
+    public String testApi(@RequestBody String message) {
+        log.info("API 수신 했습니다.");
+        producer.sendMessage("Jun 님이 message 보냅니다");
+        return "api 통신 성공";
+    }
+}
