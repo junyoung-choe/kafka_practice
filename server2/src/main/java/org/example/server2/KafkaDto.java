fd5f7c3381a6471fe3b259cd46c005ae9e5892b1
최준영 <wjdghkswnsdu@naver.com>
2024-05-14 13:10:22 +0900

    fix: object mapper 기본 생성자 에러 fix

--- server2/src/main/java/org/example/server2/KafkaDto.java
+++ server2/src/main/java/org/example/server2/KafkaDto.java
@@ -2,9 +2,11 @@ package org.example.server2;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class KafkaDto {
